8f55749599cd47f727579fa08fb0e323c2f37e9a
jaydenaleung <139849747+jaydenaleung@users.noreply.github.com>
2024-11-23 11:06:22 -0500

    Update classes.py

--- classes.py
+++ classes.py
@@ -25,3 +25,13 @@ class Enemy(Entity):
         
     def update(self,surface):
         surface.blit(self.image, (self.x, self.y))
+
+class Scene:
+    def __init__(self, imagePath, *coords: tuple[int,int,int,int]):
+        self.image = pygame.image.load(imagePath)
+        
+        for index in coords:
+            self.x1 = coords[0]
+            self.y1 = coords[1]
+            self.x2 = coords[2]
+            self.y2 = coords[3]
