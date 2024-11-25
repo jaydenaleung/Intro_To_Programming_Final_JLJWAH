36e607cb5824e83246363cc5313ee3d950402120
jaydenaleung <139849747+jaydenaleung@users.noreply.github.com>
2024-11-25 00:31:27 -0500

    Added example barrier
    I FINALLY DID IT IT TOOK ME 2 HOURS

--- main.py
+++ main.py
@@ -19,10 +19,14 @@ background = "white"
 # CLASS OBJECTS (CHARACTERS)
 player = classes.Player(300,300,"assets\example_player.png")
 
+# EXAMPLE CODE FOR SCENE (WILL REMOVE LATER)
+example_background = pygame.image.load("assets\example_battlefield.png")
+# Top Platform: (550,102),(733,124) - topleft,bottomRight
 
 while running:
     # SETUP
     screen.fill(background)
+    screen.blit(example_background,(0,0))
 
 
     # WATCH FOR EVENTS HERE (CONDITIONALS SECTION)
@@ -47,20 +51,35 @@ while running:
             player.movingRight = False
             player.movingUp = False
             player.movingDown = False
+        
+        if event.type == pygame.MOUSEBUTTONUP:
+            print(str(pygame.mouse.get_pos()[0]) + "," + str(pygame.mouse.get_pos()[1]))
+
     
     if player.movingLeft and player.x >= 0: # Movement logic - without this, it will only move once when pressed and not when pressed down
-        player.x -= player.speed
-    if player.movingRight and player.x + player.image.get_size()[0] <= resX: # playerImg.get_size()[0] gets the horizontal size of the player
-        player.x += player.speed
+        if (not (player.x > 550 and player.x < 733 and player.y > 102-player.sizey and player.y < 124)): # Top platform/example
+            player.x -= player.speed
+            if player.x > 550 and player.x <= 733 and player.y > 102-player.sizey and player.y < 124:
+                player.x = 733
+    if player.movingRight and player.x + player.sizex <= resX: # playerImg.get_size()[0] gets the horizontal size of the player
+        if (not (player.x + player.sizex > 550 and player.x < 733 and player.y > 102-player.sizey and player.y < 124)):
+            player.x += player.speed
+            if player.x < 733 and player.x + player.sizex >= 550 and player.y > 102-player.sizey and player.y < 124:
+                player.x = 550-player.sizex
     if player.movingUp and player.y >= 0:
-        player.y -= player.speed
-    if player.movingDown and player.y + player.image.get_size()[1] <= resY:
-        player.y += player.speed
+        if (not (player.y > 102 and player.y < 124 and player.x > 550-player.sizex and player.x < 733)):
+            player.y -= player.speed
+            if player.y <= 124 and player.y > 102 and player.x > 550-player.sizex and player.x < 733:
+                player.y = 125
+    if player.movingDown and player.y + player.sizey <= resY:
+        if (not (player.y + player.sizey > 102 and player.y < 124 and player.x > 550-player.sizex and player.x < 733)):
+            player.y += player.speed
+            if player.y < 124 and player.y + player.sizey >= 102 and player.x > 550-player.sizex and player.x < 733:
+                player.y = 101-player.sizey
 
 
     # RENDER GAME OBJECTS HERE 
     player.update(screen)
-    
 
     # UPDATE SCREEN
     pygame.display.flip()
