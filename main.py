1a6585b6adccd8b827331c339547ff1ed163d928
jaydenaleung <139849747+jaydenaleung@users.noreply.github.com>
2024-12-08 20:47:53 -0500

    Added respawn and less gravity

--- main.py
+++ main.py
@@ -27,11 +27,12 @@ background = "white"
 # Characters/player animations
 mario = classes.Character(
     # Animations
-    ["assets\characters\example_char.png"],
+    ["assets\characters\Mario\Mario_right1.png"],
 
     # Moves
     [classes.Shield(screen),
-     classes.Punch(screen,5)]
+     classes.Punch(screen,5),
+     classes.Fireball(screen,10)]
 )
 
 amogus = classes.Character(
@@ -63,21 +64,53 @@ amogus = classes.Character(
 characters = [mario,amogus] # more to come
 
 # CLASS OBJECTS
-spawnX1 = 280; spawnY1 = 294; spawnX2 = 970; spawnY2 = 294
-player = classes.Player(spawnX1,spawnY1,characters[0])
-enemy = classes.Enemy(spawnX2,spawnY2,characters[1])
-players = [player,enemy]
+## Backgrounds
+b='b'; default = classes.Scene(f"assets\{b}ackgrounds\default_battlefield.png")
+default.spawnX1 = 280; default.spawnY1 = 294; default.spawnX2 = 970; default.spawnY2 = 294
+barrierT_1 = default.Barrier(550,102,733,124)
+barrierL_1 = default.Barrier(340,218,528,238)
+barrierR_1 = default.Barrier(756,218,944,237)
+barrierM1_1 = default.Barrier(252,355,1016,412)
+#barrierM2_1 = default.Barrier(311,317,913,458)
+barrierM3_1 = default.Barrier(375,461,890,484)
+barrierM4_1 = default.Barrier(435,494,772,546)
+barrierM5_1 = default.Barrier(587,555,711,657)
+default.barriers = [barrierT_1,barrierL_1,barrierR_1,barrierM1_1,barrierM3_1,barrierM4_1,barrierM5_1]
 
-scene = classes.Scene("assets\example_battlefield.png")
-barrierT = scene.Barrier(550,102,733,124)
-barrierL = scene.Barrier(340,218,528,238)
-barrierR = scene.Barrier(756,218,944,237)
-barrierM1 = scene.Barrier(252,355,1016,412)
-#barrierM2 = scene.Barrier(311,317,913,458)
-barrierM3 = scene.Barrier(375,461,890,484)
-barrierM4 = scene.Barrier(435,494,772,546)
-barrierM5 = scene.Barrier(587,555,711,657)
-barriers = [barrierT,barrierL,barrierR,barrierM1,barrierM3,barrierM4,barrierM5]
+b='b';f='f';finalDest = classes.Scene(f"assets\{b}ackgrounds\{f}inal_destination.jpg")
+finalDest.spawnX1 = 405; finalDest.spawnY1 = 443; finalDest.spawnX2 = 835; finalDest.spawnY2 = 443
+barrierM1_2 = finalDest.Barrier(354,484,929,535)
+barrierM2_2 = finalDest.Barrier(401,543,880,612)
+barrierM3_2 = finalDest.Barrier(532,621,727,685)
+finalDest.barriers = [barrierM1_2,barrierM2_2,barrierM3_2]
+
+b='b';f='f';fountain = classes.Scene(f"assets\{b}ackgrounds\{f}ountain_of_dreams.jpg")
+fountain.spawnX1 = 400; fountain.spawnY1 = 448; fountain.spawnX2 = 860; fountain.spawnY2 = 448
+barrierT_3 = fountain.Barrier(573,298,706,308)
+barrierR_3 = fountain.Barrier(759,402,893,411)
+barrierL_3 = fountain.Barrier(387,402,521,411)
+barrierC1_3 = fountain.Barrier(574,394,707,437)
+barrierM1_3 = fountain.Barrier(350,489,923,716)
+fountain.barriers = [barrierT_3,barrierR_3,barrierL_3,barrierC1_3,barrierM1_3]
+
+b='b';f='f';luigi = classes.Scene(f"assets\{b}ackgrounds\luigi_mansion.jpg")
+luigi.spawnX1 = 300; luigi.spawnY1 = 459; luigi.spawnX2 = 950; luigi.spawnY2 = 459
+barrierT_4 = fountain.Barrier(345,370,938,387)
+barrierL_4 = fountain.Barrier(537,445,599,458)
+barrierR_4 = fountain.Barrier(684,447,741,457)
+barrierM1_4 = fountain.Barrier(246,500,1039,533)
+barrierM2_4 = fountain.Barrier(229,540,1037,713)
+luigi.barriers = [barrierT_4,barrierL_4,barrierR_4,barrierM1_4,barrierM2_4]
+
+scenes = [default,finalDest,fountain,luigi]
+selectedScene = scenes[1]
+
+## Players
+spawnX1 = selectedScene.spawnX1; spawnY1 = selectedScene.spawnY1; spawnX2 = selectedScene.spawnX2; spawnY2 = selectedScene.spawnY2
+player = classes.Player(spawnX1,spawnY1,characters[0])
+player2 = classes.Player(spawnX2,spawnY2,characters[1])
+players = [player,player2]
+
 
 if 脑子 == "smart":
     脑子 = "Found"
@@ -85,7 +118,7 @@ if 脑子 == "smart":
 while running:
     # SETUP
     screen.fill(background)
-    scene.update(screen)
+    selectedScene.update(screen)
 
 
     # WATCH FOR EVENTS HERE (CONDITIONALS SECTION)
@@ -95,8 +128,8 @@ while running:
         functions.attackCheck(event,players[0],players[1])
     
     for character in players:
-        for barrier in barriers:
-            barrier.solidify(screen,character)
+        for barrier in selectedScene.barriers:
+            barrier.solidify(screen,character,players)
 
 
     # RENDER GAME OBJECTS HERE
