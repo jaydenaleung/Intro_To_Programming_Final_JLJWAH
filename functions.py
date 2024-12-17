80bfaeee71532498c07cdfc3a07b78808d08be94
jaydenaleung <139849747+jaydenaleung@users.noreply.github.com>
2024-12-17 00:33:43 -0500

    Update functions.py

--- functions.py
+++ functions.py
@@ -6,7 +6,7 @@ To access the functions here in another file, use 'import functions'.
 
 - Joza, Amie, Jayden: Intro to Programming Fall 2024
 '''
-
+#quit the game
 def quitCheck(event):
     if event.type == pygame.KEYDOWN:
         if event.key == pygame.K_ESCAPE:
@@ -46,7 +46,7 @@ def moveCheck(event,player,player2):
 
         if event.key == pygame.K_RIGHT: # move right
             player2.movingRight = True
-  
+    # same thing as above just WASD
     elif event.type == pygame.KEYUP:
         if event.key == pygame.K_a:
             player.movingLeft = False
@@ -64,7 +64,7 @@ def moveCheck(event,player,player2):
         print(str(pygame.mouse.get_pos()[0]) + "," + str(pygame.mouse.get_pos()[1]))
         print(str(player.x)+","+str(player.y))
 
-def attackCheck(event,player,player2):
+def attackCheck(event,player,player2): # move detecting logic
     if event.type == pygame.KEYDOWN:
         movesP = player.chosenCharacter.moves
         for move in movesP:
@@ -74,8 +74,11 @@ def attackCheck(event,player,player2):
                     player.move2 = 'melee'
             elif move == 'ranged':
                 if event.key == pygame.K_c:
-                    player.move3Activated = not player.move3Activated
+                    player.move3Activated = True
+                    player.renderNewProj = True
                     player.move3 = 'ranged'
+                else:
+                    player.renderNewProj = False
             elif move == 'support': # more moves to come
                 if event.key == pygame.K_z:
                     player.move1Activated = True
@@ -84,28 +87,39 @@ def attackCheck(event,player,player2):
                 if event.key==pygame.K_v and player.ultUse:
                     player.move4Activated = True
                     player.move4 = 'ult'
-                    player.doSaveHP = True
+                    if move.__class__.__name__ == "Vent":
+                        player.doSaveHP = True
+                    else:
+                        player.doSaveHP = False
             else:
                 player.move1Activated = False
 
         movesE = player2.chosenCharacter.moves
         for move in movesE:
-            if move == 'melee': # moves to come  
+            if move == 'melee':
                 if event.key == pygame.K_m:
                     player2.move2Activated = not player2.move2Activated
                     player2.move2 = 'melee'
-            elif move == 'ranged': # moves to come
+            elif move == 'ranged':
                 if event.key == pygame.K_COMMA:
-                    player2.move3Activated = not player2.move3Activated
+                    player2.move3Activated = True
+                    player2.renderNewProj = True
                     player2.move3 = 'ranged'
+                else:
+                    player2.renderNewProj = False
             elif move == 'support': # more moves to come
                 if event.key == pygame.K_n:
                     player2.move1Activated = True
                     player2.move1 = 'support'
             elif move == 'ult':
                 if event.key==pygame.K_PERIOD and player2.ultUse:
-                    player2.move4Activated = not player2.move4Activated
+                    player2.move4Activated = True
                     player2.move4 = 'ult'
-                    player2.doSaveHP = True
+                    if player2.chosenCharacter.moves['ult'][0].__class__.__name__ == "Vent":
+                        player2.doSaveHP = True
+                        print("True")
+                    else:
+                        player2.doSaveHP = False
+                        print("false")
             else:
-                player.move1Activated = False
+                player2.move1Activated = False
